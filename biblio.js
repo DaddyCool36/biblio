7e3f5ea1d7951bc9d6aafec7a15907266ff4082f
Froum <djfroum@gmail.com>
2024-12-11 10:02:11 +0100

    Etiquette type 3

--- biblio.js
+++ biblio.js
@@ -25,7 +25,7 @@ var Livre = function(){
 		ctx.fillRect(this.position.x, this.position.y, this.dimensions.epaisseur, -this.dimensions.hauteur);
 
         // etiquettes
-        let typeEtiquette = 2;
+        let typeEtiquette = Math.floor(Math.random()*4);
 
         ctx.fillStyle = "rgba(255,255,255,0.5)";
         let marge = 3;
@@ -42,6 +42,11 @@ var Livre = function(){
             case 2:
                 ctx.fillRect(this.position.x + marge, this.position.y - 8, this.dimensions.epaisseur - (2 * marge), -this.dimensions.hauteur + 25);
                 ctx.fillRect(this.position.x + marge, this.position.y - this.dimensions.hauteur + marge, this.dimensions.epaisseur - (2 * marge), 3);
+                break;
+            case 3:
+                ctx.fillRect(this.position.x + marge, this.position.y - this.dimensions.hauteur + marge, this.dimensions.epaisseur - (2 * marge), 3);
+                ctx.fillRect(this.position.x + marge, this.position.y - 12, this.dimensions.epaisseur - (2 * marge), -this.dimensions.hauteur + 25);
+                ctx.fillRect(this.position.x + marge, this.position.y - marge, this.dimensions.epaisseur - (2 * marge), - 5);
                 break;
             default:
                 break;
